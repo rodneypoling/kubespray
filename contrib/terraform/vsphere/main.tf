1d3ffbd026927093d4ffb3ceef67d8e8814aa935
Rodney Poling <rcpoling@gmail.com>
2024-03-01 20:40:38 -0500

    update

--- contrib/terraform/vsphere/main.tf
+++ contrib/terraform/vsphere/main.tf
@@ -139,7 +139,7 @@ data "template_file" "haproxy_backend" {
 
   vars = {
     prefix_server     = "${var.prefix}"
-    backend_server_ip = "${lookup(module.kubernetes.master_ip, count.index)}"
+    backend_server_ip = "${lookup(module.kubernetes.master_ip)}"
     count             = "${count.index}"
   }
 }
